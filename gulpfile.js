99452cebfe0b0965e9ea365f58d7b177bc929cff
chengj <ab140140@163.com>
2019-12-19 09:23:27 +0800

    feat: sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,18 @@ const gulp = require('gulp'),
     concat = require('gulp-concat'),
     cleanCss = require('gulp-clean-css'),
     header = require('gulp-header'),
-    reanme = require('gulp-rename'),
+    rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefix = require('gulp-autoprefixer'),
+    gulpif = require('gulp-if'),
     fs = require("fs"),
     del = require('del'),
     theme = process.env.npm_config_theme || 'default',
+    argv = require('yargs').argv,
+    node_env = argv.env || 'development',
     scss_path = ['src/**/*.scss', '!node_modules'],
     output_path = 'dist/';
+
 
 const scssTask = () => {
     gulp.src([...scss_path, '!src/theme/*.scss'])
@@ -18,20 +22,20 @@ const scssTask = () => {
         .on('error', scss.logError)//错误信息
         .pipe(setGlobalScss())
         .pipe(scss())
-        .pipe(cleanCss())
+        .pipe(gulpif(node_env === 'production', cleanCss())) // 仅在生产环境时候进行压缩
         .pipe(autoprefix())
-        .pipe(reanme((path) => {
-            return {
+        .pipe(rename((path) => {
+            return{
                 dirname: path.dirname,
                 basename: path.basename,
                 suffix: '.min',
                 extname: ".css"
             }
         }))
-        .pipe(sourcemaps.write('.'))
+        // .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(`${output_path}/style/modules/`))
-        .pipe(concat(`${theme}.min.css`))
-        .pipe(sourcemaps.write('.'))
+        .pipe(concat(`${theme}${node_env === 'production'?'.min':''}.css`))
+        // .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(`${output_path}/style/`))
 }
 
