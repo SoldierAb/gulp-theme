d72946d87a923f637027242b439be1603b5944f3
chengj <ab140140@163.com>
2019-12-22 15:32:34 +0800

    feat: update config

--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ module.exports = {
     common: './src/theme/common.scss',
     theme: {
         default: './src/theme/default.scss',
-        light: './src/theme/default.scss',
+        light: './src/theme/light.scss',
     },
     themeTagId:'theme',
     output:{
